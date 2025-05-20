34da3f486228d8206f70e2c5b32019503f9202e1
escramer <esc90cramer@yahoo.com>
2025-05-20 13:51:25 -0700

    Add a dropdown and button

--- main.py
+++ main.py
@@ -1,17 +1,6 @@
 import streamlit as st
 
-st.title("Hello Stramlit-er 👋")
-st.markdown(
-    """ 
-    This is a playground for you to try Streamlit and have fun. 
-
-    **There's :rainbow[so much] you can build!**
-    
-    We prepared a few examples for you to get started. Just 
-    click on the buttons above and discover what you can do 
-    with Streamlit. 
-    """
-)
-
-if st.button("Send balloons!"):
+mood = st.selectbox('What is your mood?', ['😊', '😠', '😕', '🎉'])
+if st.button('Submit'):
     st.balloons()
+    
