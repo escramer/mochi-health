946781229cfc7055593e8cbc43d0468cc7f41880
escramer <esc90cramer@yahoo.com>
2025-05-21 11:34:04 -0700

    Using session_state

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ import pandas as pd
 
 # Create a connection object.
 conn = st.connection("gsheets", type=GSheetsConnection)
-df = conn.read()
-df
+if 'df' not in st.session_state:
+    st.session_state.df = conn.read()
+st.session_state.df
 
 mood = st.selectbox('What is your mood?', ['happy', 'sad', 'angry'])
 if st.button('Submit'):
     row = pd.DataFrame({'Timestamp': ['2025-05-20'], 'Mood': [mood]})
-    df = pd.concat([df, row], ignore_index=True)
-    conn.update(worksheet='Sheet1', data=df)
+    st.session_state.df = pd.concat([st.session_state.df, row], ignore_index=True)
+    conn.update(worksheet='Sheet1', data=st.session_state.df)
     #conn.reset()
-df
+st.session_state.df
