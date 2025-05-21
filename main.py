40568c3fe598672c387ba224bc5ddfc9917e17f7
escramer <esc90cramer@yahoo.com>
2025-05-20 21:30:13 -0700

    Make the row a dataframe

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ df
 
 mood = st.selectbox('What is your mood?', ['happy', 'sad', 'angry'])
 if st.button('Submit'):
-    df = pd.concat([df, pd.Series(['2025-05-20', mood], index=['Timestamp', 'Mood'])], ignore_index=True)
+    row = pd.DataFrame({'Timestamp': '2025-05-20', 'Mood': mood})
+    df = pd.concat([df, row], ignore_index=True)
     conn.update(worksheet='Sheet1', data=df)
     #conn.reset()
 
