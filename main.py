98c0621c1584dbb70db78a8c590d0cf7dc34542c
escramer <esc90cramer@yahoo.com>
2025-05-21 13:52:41 -0700

    Writing today's date

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 import pandas as pd
 import matplotlib.pyplot as plt
+from datetime import date
 
 # Create a connection object.
 conn = st.connection("gsheets", type=GSheetsConnection)
@@ -10,7 +11,7 @@ if 'df' not in st.session_state:
 
 mood = st.selectbox('What is your mood?', ['happy', 'sad', 'angry'])
 if st.button('Submit'):
-    row = pd.DataFrame({'Timestamp': ['2025-05-20'], 'Mood': [mood]})
+    row = pd.DataFrame({'Timestamp': [str(date.today())], 'Mood': [mood]})
     st.session_state.df = pd.concat([st.session_state.df, row], ignore_index=True)
     conn.update(worksheet='Sheet1', data=st.session_state.df)
 
