9a4ee247a9f57c0c09184595020e88e862c66e63
escramer <esc90cramer@yahoo.com>
2025-05-20 21:07:09 -0700

    Match parentheses

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ df = conn.read()
 
 mood = st.selectbox('What is your mood?', ['happy', 'sad', 'angry'])
 if st.button('Submit'):
-    df = pd.concat([df, pd.Series(['2025-05-20', mood])], index=['Timestamp', 'Mood']))
+    df = pd.concat([df, pd.Series(['2025-05-20', mood], index=['Timestamp', 'Mood'])])
     conn.update(worksheet='Sheet1', data=df)
     #conn.reset()
 
