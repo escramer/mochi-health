5fc81d9107e22cfcecc773611f6f4941095cbdf5
escramer <esc90cramer@yahoo.com>
2025-05-20 17:02:25 -0700

    Add an extra "o" for testing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-mood = st.selectbox('What is your mood?', ['😊', '😠', '😕', '🎉'])
+mood = st.selectbox('What is your moood?', ['😊', '😠', '😕', '🎉'])
 if st.button('Submit'):
     st.balloons()
     
