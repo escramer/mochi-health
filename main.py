195df0256744bd400aa7cc1574a7b47faeec54ae
escramer <esc90cramer@yahoo.com>
2025-05-20 21:33:03 -0700

    Each cell in the row is in its own list

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ df
 
 mood = st.selectbox('What is your mood?', ['happy', 'sad', 'angry'])
 if st.button('Submit'):
-    row = pd.DataFrame({'Timestamp': '2025-05-20', 'Mood': mood})
+    row = pd.DataFrame({'Timestamp': ['2025-05-20'], 'Mood': [mood]})
     df = pd.concat([df, row], ignore_index=True)
     conn.update(worksheet='Sheet1', data=df)
     #conn.reset()
