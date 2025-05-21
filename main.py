bce4afa51c2ab734de6830d6b3927a768487dc17
escramer <esc90cramer@yahoo.com>
2025-05-21 14:06:49 -0700

    Add confused

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ conn = st.connection("gsheets", type=GSheetsConnection)
 if 'df' not in st.session_state:
     st.session_state.df = conn.read()
 
-mood = st.selectbox('What is your mood?', ['happy', 'sad', 'angry'])
+mood = st.selectbox('What is your mood?', ['happy', 'sad', 'angry', 'confused'])
 if st.button('Submit'):
     row = pd.DataFrame({'Timestamp': [str(date.today())], 'Mood': [mood]})
     st.session_state.df = pd.concat([st.session_state.df, row], ignore_index=True)
